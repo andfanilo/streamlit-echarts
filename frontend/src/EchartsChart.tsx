786f793197af37eef27ca93f19b442259450f276
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-05-10 18:42:15 +0200

    :construction: Clean construction of theme.

--- frontend/src/EchartsChart.tsx
+++ frontend/src/EchartsChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { ComponentProps, Streamlit, withStreamlitConnection } from "./streamlit"
 import * as _ from "lodash"
 
@@ -8,21 +8,22 @@ import ReactEcharts from "echarts-for-react"
 import "./streamlit.css"
 
 const EchartsChart = (props: ComponentProps) => {
-  useEffect(() => {
-    Streamlit.setFrameHeight()
-  })
-
   /**
-   * If theme prop is an object, register it under custom_theme name
+   * Deal with theme
+   * If theme prop is an object, register it under "custom_theme" name
+   * If it's a string just return it
+   * If it's None, ECharts component will take care of it
    */
-  const themeProp = props.args["theme"]
-  const customThemeName = "custom_theme"
-  if (_.isObject(themeProp)) {
-    echarts.registerTheme(customThemeName, themeProp)
+  const getThemeName = (themeProp: string | object) => {
+    const customThemeName = "custom_theme"
+    if (_.isObject(themeProp)) {
+      echarts.registerTheme(customThemeName, themeProp)
+    }
+    return _.isObject(props.args["theme"])
+      ? customThemeName
+      : props.args["theme"]
   }
-  const themeName = _.isObject(props.args["theme"])
-    ? customThemeName
-    : props.args["theme"]
+  const themeName = getThemeName(props.args["theme"])
 
   /*
   const mapFunctions = (obj: object) => {
@@ -45,7 +46,17 @@ const EchartsChart = (props: ComponentProps) => {
   options["series"][1]["itemStyle"]["color"] = fn
    */
   const options = props.args["options"]
-  return <ReactEcharts option={options} theme={themeName} />
+  return (
+    <>
+      <ReactEcharts
+        option={options}
+        theme={themeName}
+        onChartReady={() => {
+          Streamlit.setFrameHeight()
+        }}
+      />
+    </>
+  )
 }
 
 export default withStreamlitConnection(EchartsChart)
