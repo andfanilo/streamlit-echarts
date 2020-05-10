e3bbbd175d8d97940e6e0d708b4a8be9764dcd89
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-05-10 18:25:51 +0200

    :construction: Change to functional component.

--- frontend/src/EchartsChart.tsx
+++ frontend/src/EchartsChart.tsx
@@ -1,25 +1,35 @@
-import React from "react"
-import {
-  ComponentProps,
-  StreamlitComponentBase,
-  withStreamlitConnection,
-} from "./streamlit"
+import React, { useEffect } from "react"
+import { ComponentProps, Streamlit, withStreamlitConnection } from "./streamlit"
 import * as _ from "lodash"
-import deepMapValues from "./utils"
 
 import echarts from "echarts"
 import ReactEcharts from "echarts-for-react"
 
 import "./streamlit.css"
 
-class EchartsChart extends StreamlitComponentBase {
-  private customThemeName = "custom_theme"
+const EchartsChart = (props: ComponentProps) => {
+  useEffect(() => {
+    Streamlit.setFrameHeight()
+  })
 
-  private mapFunctions = (obj: object) => {
+  /**
+   * If theme prop is an object, register it under custom_theme name
+   */
+  const themeProp = props.args["theme"]
+  const customThemeName = "custom_theme"
+  if (_.isObject(themeProp)) {
+    echarts.registerTheme(customThemeName, themeProp)
+  }
+  const themeName = _.isObject(props.args["theme"])
+    ? customThemeName
+    : props.args["theme"]
+
+  /*
+  const mapFunctions = (obj: object) => {
     // Map over all strings in options, cast as functions when found
     let funcReg = /function *\(([^()]*)\)[ \n\t]*{(.*)}/gim
 
-    return deepMapValues(obj, function (v: string) {
+    return deepMapValues(obj, function(v: string) {
       let match = funcReg.exec(v.replace(/\n/g, " "))
       if (match) {
         // @ts-ignore
@@ -30,28 +40,12 @@ class EchartsChart extends StreamlitComponentBase {
     })
   }
 
-  public constructor(props: ComponentProps) {
-    super(props)
-
-    const themeProp = props.args["theme"]
-    if (_.isObject(themeProp)) {
-      echarts.registerTheme(this.customThemeName, themeProp)
-    }
-  }
-
-  public render = (): React.ReactNode => {
-    const options = this.props.args["options"]
-    const themeName = _.isObject(this.props.args["theme"])
-      ? this.customThemeName
-      : this.props.args["theme"]
-
-    /*
-    let fn = options["series"][0]["itemStyle"]["color"]
-    fn = new Function("return " + fn)()
-    options["series"][1]["itemStyle"]["color"] = fn
-     */
-    return <ReactEcharts option={options} theme={themeName} />
-  }
+  let fn = options["series"][0]["itemStyle"]["color"]
+  fn = new Function("return " + fn)()
+  options["series"][1]["itemStyle"]["color"] = fn
+   */
+  const options = props.args["options"]
+  return <ReactEcharts option={options} theme={themeName} />
 }
 
 export default withStreamlitConnection(EchartsChart)
