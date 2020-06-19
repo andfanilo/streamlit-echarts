3a20d178c1ea07eb98897ffb3f7dcac4c2f91379
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-06-19 15:08:00 +0200

    :books: Manage examples with selectbox

--- examples/app.py
+++ examples/app.py
@@ -3,93 +3,118 @@ from pyecharts.commons.utils import JsCode
 
 from streamlit_echarts import st_echarts
 
-st.header("Hello ECharts !")
 
-st.subheader("Basic rendering")
-st_echarts(
-    options={
-        "xAxis": {
-            "type": "category",
-            "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        },
-        "yAxis": {"type": "value"},
-        "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
+def main():
+    PAGES = {
+        "Basic rendering": render_basic,
+        "Custom pie chart": render_custom_pie,
+        "Datazoom": render_datazoom,
     }
-)
 
-st.subheader("Customized pie chart")
-pie_options = {
-    "backgroundColor": "#2c343c",
-    "title": {
-        "text": "Customized Pie",
-        "left": "center",
-        "top": 20,
-        "textStyle": {"color": "#ccc"},
-    },
-    "tooltip": {"trigger": "item", "formatter": "{a} <br/>{b} : {c} ({d}%)"},
-    "visualMap": {
-        "show": False,
-        "min": 80,
-        "max": 600,
-        "inRange": {"colorLightness": [0, 1]},
-    },
-    "series": [
-        {
-            "name": "Source of interview",
-            "type": "pie",
-            "radius": "55%",
-            "center": ["50%", "50%"],
-            "data": [
-                {"value": 235, "name": "Video Ad"},
-                {"value": 274, "name": "Affiliate Ad"},
-                {"value": 310, "name": "Email marketing"},
-                {"value": 335, "name": "Direct access"},
-                {"value": 400, "name": "Search engine"},
+    st.title("Hello ECharts !")
+    st.sidebar.header("Configuration")
+    page = st.sidebar.selectbox("Choose an example", options=list(PAGES.keys()))
+    PAGES[page]()
+
+
+def render_basic():
+    with st.echo("below"):
+        st_echarts(
+            options={
+                "xAxis": {
+                    "type": "category",
+                    "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                },
+                "yAxis": {"type": "value"},
+                "series": [
+                    {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+                ],
+            }
+        )
+
+
+def render_custom_pie():
+    with st.echo("below"):
+        pie_options = {
+            "backgroundColor": "#2c343c",
+            "title": {
+                "text": "Customized Pie",
+                "left": "center",
+                "top": 20,
+                "textStyle": {"color": "#ccc"},
+            },
+            "tooltip": {"trigger": "item", "formatter": "{a} <br/>{b} : {c} ({d}%)"},
+            "visualMap": {
+                "show": False,
+                "min": 80,
+                "max": 600,
+                "inRange": {"colorLightness": [0, 1]},
+            },
+            "series": [
+                {
+                    "name": "Source of interview",
+                    "type": "pie",
+                    "radius": "55%",
+                    "center": ["50%", "50%"],
+                    "data": [
+                        {"value": 235, "name": "Video Ad"},
+                        {"value": 274, "name": "Affiliate Ad"},
+                        {"value": 310, "name": "Email marketing"},
+                        {"value": 335, "name": "Direct access"},
+                        {"value": 400, "name": "Search engine"},
+                    ],
+                    "roseType": "radius",
+                    "label": {"color": "rgba(255, 255, 255, 0.3)"},
+                    "labelLine": {
+                        "lineStyle": {"color": "rgba(255, 255, 255, 0.3)"},
+                        "smooth": 0.2,
+                        "length": 10,
+                        "length2": 20,
+                    },
+                    "itemStyle": {
+                        "color": "#c23531",
+                        "shadowBlur": 200,
+                        "shadowColor": "rgba(0, 0, 0, 0.5)",
+                    },
+                    "animationType": "scale",
+                    "animationEasing": "elasticOut",
+                }
             ],
-            "roseType": "radius",
-            "label": {"color": "rgba(255, 255, 255, 0.3)"},
-            "labelLine": {
-                "lineStyle": {"color": "rgba(255, 255, 255, 0.3)"},
-                "smooth": 0.2,
-                "length": 10,
-                "length2": 20,
-            },
-            "itemStyle": {
-                "color": "#c23531",
-                "shadowBlur": 200,
-                "shadowColor": "rgba(0, 0, 0, 0.5)",
-            },
-            "animationType": "scale",
-            "animationEasing": "elasticOut",
         }
-    ],
-}
-st_echarts(options=pie_options)
+        st_echarts(options=pie_options)
 
-st.subheader("With data zoom + JsCode")
-data = [
-    ["14.616", "7.241", "0.896"],
-    ["3.958", "5.701", "0.955"],
-    ["2.768", "8.971", "0.669"],
-    ["9.051", "9.710", "0.171"],
-    ["14.046", "4.182", "0.536"],
-    ["12.295", "1.429", "0.962"],
-    ["4.417", "8.167", "0.113"],
-    ["0.492", "4.771", "0.785"],
-    ["7.632", "2.605", "0.645"],
-    ["14.242", "5.042", "0.368"],
-]
-option_js = {
-    "xAxis": {"type": "value"},
-    "yAxis": {"type": "value"},
-    "dataZoom": [{"type": "slider", "start": 10, "end": 60}],
-    "series": [
-        {
-            "type": "scatter",
-            "itemStyle": {"opacity": 0.8},
-            "symbolSize": JsCode("""function (val) {  return val[2] * 40; }""").js_code,
-            "data": data,
+
+def render_datazoom():
+    with st.echo("below"):
+        data = [
+            ["14.616", "7.241", "0.896"],
+            ["3.958", "5.701", "0.955"],
+            ["2.768", "8.971", "0.669"],
+            ["9.051", "9.710", "0.171"],
+            ["14.046", "4.182", "0.536"],
+            ["12.295", "1.429", "0.962"],
+            ["4.417", "8.167", "0.113"],
+            ["0.492", "4.771", "0.785"],
+            ["7.632", "2.605", "0.645"],
+            ["14.242", "5.042", "0.368"],
+        ]
+        option_js = {
+            "xAxis": {"type": "value"},
+            "yAxis": {"type": "value"},
+            "dataZoom": [{"type": "slider", "start": 10, "end": 60}],
+            "series": [
+                {
+                    "type": "scatter",
+                    "itemStyle": {"opacity": 0.8},
+                    "symbolSize": JsCode(
+                        """function (val) {  return val[2] * 40; }"""
+                    ).js_code,
+                    "data": data,
+                }
+            ],
         }
-    ],
-}
-st_echarts(options=option_js)
+        st_echarts(options=option_js)
+
+
+if __name__ == "__main__":
+    main()
--- examples/app_pyecharts.py
+++ examples/app_pyecharts.py
@@ -4,90 +4,165 @@ import streamlit as st
 from pyecharts import options as opts
 from pyecharts.charts import Bar
 from pyecharts.charts import Timeline
+from pyecharts.commons.utils import JsCode
 from pyecharts.faker import Faker
 
 from streamlit_echarts import st_pyecharts
 
-st.header("Hello Pyecharts !")
 
-st.subheader("Basic rendering")
-b = (
-    Bar()
-    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-    .add_yaxis("2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2])
-    .set_global_opts(
-        title_opts=opts.TitleOpts(
-            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-        ),
-        toolbox_opts=opts.ToolboxOpts(),
-    )
-)
-st_pyecharts(b)
+def main():
+    PAGES = {
+        "Basic rendering": render_basic,
+        "Custom themes": render_custom,
+        "Filter with legend": render_filter_legend,
+        "Timeline": render_timeline,
+        "Chart with randomization": render_randomize,
+    }
 
-st.subheader("With custom theme")
-b = (
-    Bar()
-    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-    .add_yaxis("2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2])
-    .set_global_opts(
-        title_opts=opts.TitleOpts(
-            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-        )
-    )
-)
-st_pyecharts(b, theme="dark")
+    st.header("Hello Pyecharts !")
+    st.sidebar.header("Configuration")
+    page = st.sidebar.selectbox("Choose an example", options=list(PAGES.keys()))
+    PAGES[page]()
 
-st_pyecharts(
-    b,
-    theme={
-        "backgroundColor": "#f4cccc",
-        "textStyle": {"color": "rgba(255, 0, 0, 0.8)"},
-    },
-)
 
-st.subheader("With animation")
-c = (
-    Bar(
-        init_opts=opts.InitOpts(
-            animation_opts=opts.AnimationOpts(
-                animation_delay=1000, animation_easing="elasticOut"
+def render_basic():
+    with st.echo("below"):
+        b = (
+            Bar()
+            .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+            .add_yaxis(
+                "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(
+                    title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+                ),
+                toolbox_opts=opts.ToolboxOpts(),
             )
         )
-    )
-    .add_xaxis(Faker.choose())
-    .add_yaxis("商家A", Faker.values())
-    .add_yaxis("商家B", Faker.values())
-    .set_global_opts(title_opts=opts.TitleOpts(title="Bar-动画配置基本示例", subtitle="我是副标题"))
-)
-st_pyecharts(c)
+        st_pyecharts(b)
 
-st.subheader("With timeline")
-x = Faker.choose()
-tl = Timeline()
-for i in range(2015, 2020):
-    bar = (
-        Bar()
-        .add_xaxis(x)
-        .add_yaxis("商家A", Faker.values())
-        .add_yaxis("商家B", Faker.values())
-        .set_global_opts(title_opts=opts.TitleOpts("某商店{}年营业额".format(i)))
-    )
-    tl.add(bar, "{}年".format(i))
-st_pyecharts(tl)
 
-st.subheader("With a button to push random data")
-b = (
-    Bar()
-    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-    .add_yaxis("2017-2018 Revenue in (billion $)", random.sample(range(100), 10))
-    .set_global_opts(
-        title_opts=opts.TitleOpts(
-            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-        ),
-        toolbox_opts=opts.ToolboxOpts(),
-    )
-)
-st_pyecharts(
-    b, key="echarts"
-)  # Add key argument to not remount component at every Streamlit run
-st.button("Randomize data")
+def render_custom():
+    with st.echo("below"):
+        b = (
+            Bar()
+            .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+            .add_yaxis(
+                "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(
+                    title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+                )
+            )
+        )
+        st_pyecharts(b, theme="dark")
+
+        st_pyecharts(
+            b,
+            theme={
+                "backgroundColor": "#f4cccc",
+                "textStyle": {"color": "rgba(255, 0, 0, 0.8)"},
+            },
+        )
+
+
+def render_filter_legend():
+    with st.echo("below"):
+        c = (
+            Bar(
+                init_opts=opts.InitOpts(
+                    animation_opts=opts.AnimationOpts(
+                        animation_delay=1000, animation_easing="elasticOut"
+                    )
+                )
+            )
+            .add_xaxis(Faker.choose())
+            .add_yaxis("商家A", Faker.values())
+            .add_yaxis("商家B", Faker.values())
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="Bar-动画配置基本示例", subtitle="我是副标题")
+            )
+        )
+        st_pyecharts(c)
+
+
+def render_timeline():
+    with st.echo("below"):
+        x = Faker.choose()
+        tl = Timeline()
+        for i in range(2015, 2020):
+            bar = (
+                Bar()
+                .add_xaxis(x)
+                .add_yaxis("商家A", Faker.values())
+                .add_yaxis("商家B", Faker.values())
+                .set_global_opts(title_opts=opts.TitleOpts("某商店{}年营业额".format(i)))
+            )
+            tl.add(bar, "{}年".format(i))
+        st_pyecharts(tl)
+
+
+def render_randomize():
+    with st.echo("below"):
+        b = (
+            Bar()
+            .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+            .add_yaxis(
+                "2017-2018 Revenue in (billion $)", random.sample(range(100), 10)
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(
+                    title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+                ),
+                toolbox_opts=opts.ToolboxOpts(),
+            )
+        )
+        st_pyecharts(
+            b, key="echarts"
+        )  # Add key argument to not remount component at every Streamlit run
+        st.button("Randomize data")
+
+
+def render_js():
+    with st.echo("below"):
+        st.markdown(
+            """Overwrite chart colors with JS. 
+        Under 50 : red. Between 50 - 100 : blue. Over 100 : green"""
+        )
+        color_function = """
+                function (params) {
+                    if (params.value > 0 && params.value < 50) {
+                        return 'red';
+                    } else if (params.value > 50 && params.value < 100) {
+                        return 'blue';
+                    }
+                    return 'green';
+                }
+                """
+        c = (
+            Bar()
+            .add_xaxis(Faker.choose())
+            .add_yaxis(
+                "商家A",
+                Faker.values(),
+                itemstyle_opts=opts.ItemStyleOpts(color=JsCode(color_function)),
+            )
+            .add_yaxis(
+                "商家B",
+                Faker.values(),
+                itemstyle_opts=opts.ItemStyleOpts(color=JsCode(color_function)),
+            )
+            .add_yaxis(
+                "商家C",
+                Faker.values(),
+                itemstyle_opts=opts.ItemStyleOpts(color=JsCode(color_function)),
+            )
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-自定义柱状颜色"))
+        )
+        st_pyecharts(c)
+
+
+if __name__ == "__main__":
+    main()
